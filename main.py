5f82aaa4ef1f435ae9bbdf8812262f720de5ef14
Olena Lysenko <o.lysenko@zakaz.global>
2023-09-29 02:10:55 +0300

    точка входу

--- main.py
+++ main.py
@@ -85,19 +85,38 @@ class AddressBook(UserDict):
 
 if __name__ == '__main__':
     
-    record1 = Record("Test1")
-    record1.add_phone("1234567890")
-    record1.add_phone("5555555555")
+    # Створення нової адресної книги
+    book = AddressBook()
 
-    record2 = Record("Test2")
-    record2.add_phone("1234567890")
-    record2.add_phone("5555555555")
-    
-    address_list = AddressBook()
-    address_list.add_record(record1)
-    address_list.add_record(record2)
+    # Створення запису для John
+    john_record = Record("John")
+    john_record.add_phone("1234567890")
+    john_record.add_phone("5555555555")
 
-    print(address_list.data)
+    # Додавання запису John до адресної книги
+    book.add_record(john_record)
+
+    # Створення та додавання нового запису для Jane
+    jane_record = Record("Jane")
+    jane_record.add_phone("9876543210")
+    book.add_record(jane_record)
+
+    # Виведення всіх записів у книзі
+    for name, record in book.data.items():
+        print(record)
+
+    # Знаходження та редагування телефону для John
+    john = book.find("John")
+    john.edit_phone("1234567890", "1112223333")
+
+    print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
+
+    # Пошук конкретного телефону у записі John
+    found_phone = john.find_phone("5555555555")
+    print(f"{john.name}: {found_phone}")  # Виведення: 5555555555
+
+    # Видалення запису Jane
+    book.delete("Jane")
 
 
 
